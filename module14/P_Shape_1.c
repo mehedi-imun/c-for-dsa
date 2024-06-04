b9c83bb53b397ea0ce85351c18a45df271164ec3
mehedi-imun <mehediimun@gmail.com>
2024-06-05 00:13:02 +0600

    print shape 1

---
+++ module14/P_Shape_1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+  int n;
+  scanf("%d", &n);
+  int k = n;
+  for (int i = 1; i <= n; i++)
+  {
+    for (int j = k; j > 0; j--)
+    {
+      printf("*");
+    }
+    printf("\n");
+    k--;
+  }
+
+  return 0;
+}
